0dfb91e5d4bd5eb0094820832bacbcc3dffc280a
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-22 19:43:03 +0530

    [Biplab] Add: Inserting details

--- AddressBookLinq/AddressBook.cs
+++ AddressBookLinq/AddressBook.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 namespace AddressBookLinq
 {
     
-    class AddressBook
+    public class AddressBook
     {
-        public void CreateAddressBookDataTable()
+        DataTable dataTable = new DataTable();
+        public AddressBook()
         {
-            DataTable dataTable = new DataTable();
             dataTable.Columns.Add("FirstName", typeof(string));
             dataTable.Columns.Add("LastName", typeof(string));
             dataTable.Columns.Add("Address", typeof(string));
@@ -22,6 +22,29 @@ namespace AddressBookLinq
             dataTable.Columns.Add("PhoneNumber", typeof(long));
             dataTable.Columns.Add("Email", typeof(string));
         }
+        public void AddContact()
+        {
+            dataTable.Rows.Add("Jaden", "Smith", "Street77", "New York", "America", 254632, 8563214598, "jadensmith@gmail.com");
+            dataTable.Rows.Add("Jack", "Kales", "Ohio", "Florida", "America", 789652, 8745214569, "ohiojack@gmail.com");
+            dataTable.Rows.Add("Pablo", "Escobar", "Philadelphia", "Texas", "America", 123652, 8563269547, "pablotexas@gmail.com");
+            dataTable.Rows.Add("ElChapo", "Gusman", "Hublo", "Miami", "America", 145236, 7854125632, "miamigusman@gmail.com");
+            dataTable.Rows.Add("Brandon", "Tan", "Rebublic", "Washington", "America", 145298, 7452145639, "tanbrandon@gmail.com");
+            Console.WriteLine("Contact is Added");
+        }
+        public void DisplayContacts()
+        {
+            foreach (var contact in dataTable.AsEnumerable())
+            {
+                Console.WriteLine("FirstName:" + contact.Field<string>("FirstName"));
+                Console.WriteLine("LastName:" + contact.Field<string>("LastName"));
+                Console.WriteLine("Address:" + contact.Field<string>("Address"));
+                Console.WriteLine("City:" + contact.Field<string>("City"));
+                Console.WriteLine("State:" + contact.Field<string>("State"));
+                Console.WriteLine("ZipCode:" + contact.Field<int>("Zip"));
+                Console.WriteLine("PhoneNumber:" + contact.Field<long>("PhoneNumber"));
+                Console.WriteLine("Email:" + contact.Field<string>("Email"));
+                Console.WriteLine();
+            }
+        }
     }
 }
-
--- AddressBookLinq/Program.cs
+++ AddressBookLinq/Program.cs
@@ -9,6 +9,30 @@ namespace AddressBookLinq
         {
             Console.WriteLine("Welcome To Address Book Using LINQ");
             AddressBook addressbook = new AddressBook();
+            int option = 0;
+            do
+            {
+                Console.WriteLine("1: For Add the Contact");
+                Console.WriteLine("2: For Display Contact");
+                Console.WriteLine("0: For Exist");
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addressbook.AddContact();
+                        break;
+                    case 2:
+                        addressbook.DisplayContacts();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input Please Choose Correct Option");
+                        break;
+                }
+            }
+            while (option != 0);
         }
     }
 }
